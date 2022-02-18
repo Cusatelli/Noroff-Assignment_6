bd820cf85e93ea34042f3822976620da1fa6d449
Cusatelli <einarvandevelde@gmail.com>
2022-02-18 15:01:39 +0100

    initial: initialize project.

---
+++ src/test/java/com/access_and_expose/noroffassignment_6/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.access_and_expose.noroffassignment_6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
