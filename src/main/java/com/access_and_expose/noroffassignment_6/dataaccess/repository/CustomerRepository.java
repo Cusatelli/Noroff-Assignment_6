44c4626def28544d98e2ddaa6c9bac69b35cf40b
Anna H <70937358+Haruberi@users.noreply.github.com>
2022-02-21 16:42:26 +0100

    CHORE: add methods on Interface

--- src/main/java/com/access_and_expose/noroffassignment_6/dataaccess/repository/CustomerRepository.java
+++ src/main/java/com/access_and_expose/noroffassignment_6/dataaccess/repository/CustomerRepository.java
@@ -1,7 +1,18 @@
 package com.access_and_expose.noroffassignment_6.dataaccess.repository;
 
+import com.access_and_expose.noroffassignment_6.dataaccess.model.Customer;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+
 @Repository
-public class CustomerRepository {
+public interface CustomerRepository {
+
+    ArrayList<Customer> getAllCustomers();
+    Customer getCustomerById(Long Id);
+    ArrayList<Customer> getCustomerByName(String firstName);
+    ArrayList<Customer> getCustomerByFullName(String firstName, String lastName);
+
+
+
 }
