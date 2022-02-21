8fd2fe9b89edd0d6697d57308d9f5c3b2f086b79
Anna H <70937358+Haruberi@users.noreply.github.com>
2022-02-21 09:34:02 +0100

    FEAT: add country and postalCode

--- src/main/java/com/access_and_expose/noroffassignment_6/dataaccess/service/CustomerService.java
+++ src/main/java/com/access_and_expose/noroffassignment_6/dataaccess/service/CustomerService.java
@@ -1,6 +1,7 @@
 package com.access_and_expose.noroffassignment_6.dataaccess.service;
 
 import com.access_and_expose.noroffassignment_6.dataaccess.model.Customer;
+import com.access_and_expose.noroffassignment_6.dataaccess.model.CustomerCountry;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,8 +11,8 @@ public class CustomerService {
 
     public List<Customer> getCustomers() {
         return List.of(
-                new Customer(0L, "John", "Doe", 0721234567, "johndoe@mail.com", "Country", 12345),
-                new Customer(1L, "Jane", "Doe", 0723264560, "janedoe@mail.com", "Country", 12345)
+                new Customer(0L, "John", "Doe", 0721234567, "johndoe@mail.com", new CustomerCountry("UK","AA99AA")),
+                new Customer(1L, "Jane", "Doe", 0723264560, "janedoe@mail.com",new CustomerCountry("UK","AA9AA"))
         );
     }
 }
