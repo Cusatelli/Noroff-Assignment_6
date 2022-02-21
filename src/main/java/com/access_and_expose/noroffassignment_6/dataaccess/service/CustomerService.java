a6b333d7a7250f9e844afe670cff4815ca1b39dc
Anna H <70937358+Haruberi@users.noreply.github.com>
2022-02-21 16:45:09 +0100

    REFACTOR: remove imports and spaces

--- src/main/java/com/access_and_expose/noroffassignment_6/dataaccess/service/CustomerService.java
+++ src/main/java/com/access_and_expose/noroffassignment_6/dataaccess/service/CustomerService.java
@@ -1,18 +1,15 @@
 package com.access_and_expose.noroffassignment_6.dataaccess.service;
 
+import com.access_and_expose.noroffassignment_6.dataaccess.CustomerDAO;
 import com.access_and_expose.noroffassignment_6.dataaccess.model.Customer;
-import com.access_and_expose.noroffassignment_6.dataaccess.model.CustomerCountry;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 @Service
 public class CustomerService {
 
-    public List<Customer> getCustomers() {
-        return List.of(
-                new Customer(0L, "John", "Doe", 0721234567, "johndoe@mail.com", new CustomerCountry("UK","AA99AA")),
-                new Customer(1L, "Jane", "Doe", 0723264560, "janedoe@mail.com",new CustomerCountry("UK","AA9AA"))
-        );
+    public List <Customer> getAllCustomers() {
+        CustomerDAO customerDAO = new CustomerDAO();
+        return customerDAO.getAllCustomers();
     }
 }
