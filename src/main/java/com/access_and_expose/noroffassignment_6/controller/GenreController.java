837df298957190c6162c8e583703f08f42bd8d4d
Cusatelli <einarvandevelde@gmail.com>
2022-02-22 15:37:35 +0100

    FEAT: implement Genre Repository Interface & get genre from SQLite Database.

--- src/main/java/com/access_and_expose/noroffassignment_6/controller/GenreController.java
+++ src/main/java/com/access_and_expose/noroffassignment_6/controller/GenreController.java
@@ -1,9 +1,30 @@
 package com.access_and_expose.noroffassignment_6.controller;
 
+import com.access_and_expose.noroffassignment_6.data.genre.IGenreRepository;
+import com.access_and_expose.noroffassignment_6.model.genre.Genre;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping()
+@RequestMapping(value = "api/v1/", method = RequestMethod.GET)
 public class GenreController {
+    private final IGenreRepository genreRepository;
+
+    @Autowired
+    public GenreController(IGenreRepository genreRepository) {
+        this.genreRepository = genreRepository;
+    }
+
+    @GetMapping(value = "genre/id={genreId}")
+    public Genre getGenreById(String genreId) {
+        return this.genreRepository.getGenreById(genreId);
+    }
+
+    @GetMapping(value = "genre/name={genreName}")
+    public Genre getGenreByName(String genreName) {
+        return this.genreRepository.getGenreByName(genreName);
+    }
 }
--- src/main/java/com/access_and_expose/noroffassignment_6/data/genre/GenreRepository.java
+++ src/main/java/com/access_and_expose/noroffassignment_6/data/genre/GenreRepository.java
@@ -1,15 +1,45 @@
 package com.access_and_expose.noroffassignment_6.data.genre;
 
+import com.access_and_expose.noroffassignment_6.data.SQLiteConnectionHelper;
 import com.access_and_expose.noroffassignment_6.model.genre.Genre;
+import org.springframework.stereotype.Service;
 
-public class GenreRepository implements IGenreRepository{
+import java.sql.*;
+
+@Service
+public class GenreRepository implements IGenreRepository {
+
     @Override
     public Genre getGenreById(String genreId) {
-        return null;
+        String SQLQuery = "SELECT * FROM Genre WHERE GenreId LIKE ?";
+        return getGenre(SQLQuery, genreId);
     }
 
     @Override
     public Genre getGenreByName(String genreName) {
-        return null;
+        String SQLQuery = "SELECT * FROM Genre WHERE Name LIKE ?";
+        return getGenre(SQLQuery, genreName);
+    }
+
+    private Genre getGenre(String SQLQuery, String... params) {
+        Genre genre = null;
+        try (Connection connection = DriverManager.getConnection(SQLiteConnectionHelper.getConnectionString())) {
+            PreparedStatement preparedStatement = connection.prepareStatement(SQLQuery);
+            for (int index = 1; index < params.length + 1; index++) {
+                preparedStatement.setString(index, params[index - 1]);
+            }
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                genre = new Genre(
+                        resultSet.getLong("GenreId"),
+                        resultSet.getString("Name")
+                );
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return genre;
     }
 }
