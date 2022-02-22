f2762f9946cf3fa1f07ff2b07c9637bd5566e532
Cusatelli <einarvandevelde@gmail.com>
2022-02-22 15:48:23 +0100

    FEAT: implement Track Repository Interface.

--- src/main/java/com/access_and_expose/noroffassignment_6/controller/TrackController.java
+++ src/main/java/com/access_and_expose/noroffassignment_6/controller/TrackController.java
@@ -1,9 +1,30 @@
 package com.access_and_expose.noroffassignment_6.controller;
 
+import com.access_and_expose.noroffassignment_6.data.track.ITrackRepository;
+import com.access_and_expose.noroffassignment_6.model.track.Track;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping(value = "api/v1/", method = RequestMethod.GET)
 public class TrackController {
+    private final ITrackRepository trackRepository;
+
+    @Autowired
+    public TrackController(ITrackRepository trackRepository) {
+        this.trackRepository = trackRepository;
+    }
+
+    @GetMapping(value = "track/id={trackId}")
+    public Track getTrackById(String trackId) {
+        return this.trackRepository.getTrackById(trackId);
+    }
+
+    @GetMapping(value = "track/name={trackName}")
+    public Track getTrackByName(String trackName) {
+        return this.trackRepository.getTrackByName(trackName);
+    }
 }
