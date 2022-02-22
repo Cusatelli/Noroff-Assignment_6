6472d79960ee8375aa9ca674800f5f400b9a0a91
Cusatelli <einarvandevelde@gmail.com>
2022-02-22 14:19:13 +0100

    FEAT: implement Customer Repository Interface & get customers from SQLite Database.

---
+++ src/main/java/com/access_and_expose/noroffassignment_6/data/CustomerRepository.java
@@ -0,0 +1,84 @@
+package com.access_and_expose.noroffassignment_6.data;
+
+import com.access_and_expose.noroffassignment_6.model.Customer;
+import com.access_and_expose.noroffassignment_6.model.CustomerCountry;
+import org.springframework.stereotype.Service;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+@Service
+public class CustomerRepository implements ICustomerRepository {
+
+    @Override
+    public ArrayList<Customer> getAllCustomers() {
+        String SQLQuery = "SELECT CustomerId, FirstName, LastName, Country, PostalCode, Phone, Email FROM Customer";
+        return getCustomers(SQLQuery);
+    }
+
+    @Override
+    public Customer getCustomerById(String customerId) {
+        String SQLQuery = "SELECT CustomerId, FirstName, LastName, Country, PostalCode, Phone, Email FROM Customer WHERE CustomerId = ?";
+        return getCustomers(SQLQuery, customerId).get(0);
+    }
+
+    @Override
+    public ArrayList<Customer> getCustomerByName(String firstName) {
+        String SQLQuery = "SELECT * FROM Customer WHERE FirstName LIKE ?";
+        return getCustomers(SQLQuery, firstName);
+    }
+
+    private ArrayList<Customer> getCustomers(String SQLQuery, String... params) {
+        ArrayList<Customer> customers = new ArrayList<>();
+        try (Connection connection = DriverManager.getConnection(SQLiteConnectionHelper.getConnectionString())) {
+            PreparedStatement preparedStatement = connection.prepareStatement(SQLQuery);
+            for (int index = 1; index < params.length + 1; index++) {
+                preparedStatement.setString(index, params[index - 1]);
+            }
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                CustomerCountry customerCountry = new CustomerCountry(resultSet.getString("Country"),resultSet.getString("PostalCode"));
+                Customer customer = new Customer(
+                        resultSet.getLong("CustomerId"),
+                        resultSet.getString("FirstName"),
+                        resultSet.getString("LastName"),
+                        resultSet.getInt("Phone"),
+                        resultSet.getString("Email"),
+                        customerCountry
+                );
+                customers.add(customer);
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return customers;
+    }
+
+    @Override
+    public Customer addNewCustomer(Customer customer) {
+        return null;
+    }
+
+    @Override
+    public Customer updateExistingCustomer(Customer customer) {
+        return null;
+    }
+
+    @Override
+    public boolean deleteCustomer(String id) {
+        return false;
+    }
+
+    @Override
+    public Customer getCustomerHighestSpender() {
+        return null;
+    }
+
+    @Override
+    public HashMap<CustomerCountry, Integer> getCustomersInCountry() {
+        return null;
+    }
+}
