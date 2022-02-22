f94bb7b57c1680e6df475b65edf2ad204d4a1fb2
Anna H <70937358+Haruberi@users.noreply.github.com>
2022-02-22 15:25:25 +0100

    FEAT: add controller for Genre, Artist and Track

---
+++ src/main/java/com/access_and_expose/noroffassignment_6/controller/TrackController.java
@@ -0,0 +1,9 @@
+package com.access_and_expose.noroffassignment_6.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class TrackController {
+}
