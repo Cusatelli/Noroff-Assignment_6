9f785250ff6f7b275e59634a7c5472a5587d60d1
Cusatelli <einarvandevelde@gmail.com>
2022-02-23 22:58:20 +0100

    FEAT: add Thymeleaf ViewController to get all customers as a list & add new customer (WIP).

---
+++ src/main/java/com/access_and_expose/noroffassignment_6/view/CustomerView.java
@@ -0,0 +1,47 @@
+package com.access_and_expose.noroffassignment_6.view;
+
+import com.access_and_expose.noroffassignment_6.data.repository.customer.ICustomerRepository;
+import com.access_and_expose.noroffassignment_6.model.customer.Customer;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
+
+@Controller
+@RequestMapping(value = "/customer")
+public class CustomerView {
+
+    private final ICustomerRepository customerRepository;
+
+    public CustomerView(ICustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @GetMapping("/list")
+    public String view(Model model) {
+        model.addAttribute("customers", customerRepository.getAll());
+        return "listCustomer";
+    }
+
+    @GetMapping("/add")
+    public String addView(Model model) {
+        Customer customer = new Customer();
+        model.addAttribute("customer", customer);
+        return "addCustomer";
+    }
+
+    @PostMapping("/add")
+    public String addForm(@ModelAttribute Customer customer, BindingResult errors, Model model) {
+        if (errors.hasErrors()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        customerRepository.add(customer);
+        return "addCustomer";
+    }
+}
