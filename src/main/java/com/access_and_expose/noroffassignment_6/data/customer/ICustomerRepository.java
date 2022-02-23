0cdb9694584f6689cea6940d5c35ba574a855288
Cusatelli <einarvandevelde@gmail.com>
2022-02-23 18:17:00 +0100

    STYLE: add whitespaces between imports

--- src/main/java/com/access_and_expose/noroffassignment_6/data/customer/ICustomerRepository.java
+++ src/main/java/com/access_and_expose/noroffassignment_6/data/customer/ICustomerRepository.java
@@ -1,9 +1,9 @@
 package com.access_and_expose.noroffassignment_6.data.customer;
+
 import com.access_and_expose.noroffassignment_6.model.customer.Customer;
-import com.access_and_expose.noroffassignment_6.model.customer.CustomerCountry;
 import org.springframework.stereotype.Repository;
+
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 
 @Repository
