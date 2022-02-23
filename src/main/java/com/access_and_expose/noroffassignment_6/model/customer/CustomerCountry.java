ba09903f12254f00f3b581692494013d6892f71d
Anna H <70937358+Haruberi@users.noreply.github.com>
2022-02-23 16:06:02 +0100

    REFACTOR: remove annotations

--- src/main/java/com/access_and_expose/noroffassignment_6/model/customer/CustomerCountry.java
+++ src/main/java/com/access_and_expose/noroffassignment_6/model/customer/CustomerCountry.java
@@ -1,50 +1,32 @@
 package com.access_and_expose.noroffassignment_6.model.customer;
 
-import org.springframework.data.relational.core.mapping.Table;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-
-@Entity
-@Table
 public class CustomerCountry {
 
-    @Id
-    @GeneratedValue
     private Long id;
     private String countryName;
     private String postalCode;
-
-    public CustomerCountry() {
-
-    }
+    
+    public CustomerCountry() { }
     public CustomerCountry(String countryName, String postalCode) {
         this.countryName = countryName;
         this.postalCode = postalCode;
     }
+
     public String getCountryName() {
         return countryName;
     }
-
     public void setCountryName(String countryName) {
         this.countryName = countryName;
     }
-
     public String getPostalCode() {
         return postalCode;
     }
-
     public void setPostalCode(String postalCode) {
         this.postalCode = postalCode;
     }
-
-
     public void setId(Long id) {
         this.id = id;
     }
-
-    @Id
     public Long getId() {
         return id;
     }
