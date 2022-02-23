3cc46e349dbaf3f8893ae38472e9eed8702adcf8
Anna H <70937358+Haruberi@users.noreply.github.com>
2022-02-23 16:04:42 +0100

    REFACTOR: move offset, limit on getAllCustomers from getAllCustomers in range. Implement CustomerRepository methods.

--- src/main/java/com/access_and_expose/noroffassignment_6/controller/CustomerController.java
+++ src/main/java/com/access_and_expose/noroffassignment_6/controller/CustomerController.java
@@ -2,48 +2,59 @@ package com.access_and_expose.noroffassignment_6.controller;
 
 import com.access_and_expose.noroffassignment_6.model.customer.Customer;
 import com.access_and_expose.noroffassignment_6.data.customer.ICustomerRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.*;
 import java.util.Collection;
 
 @RestController
-@RequestMapping(value = "api/v1/", method = RequestMethod.GET)
+@Tag(name = "Customer")
+@RequestMapping(value = "api/v1")
 public class CustomerController {
+
     private final ICustomerRepository customerRepository;
 
-    @Autowired
     public CustomerController(ICustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
     }
 
-    @GetMapping(value = "customers")
-    public Collection<Customer> getAllCustomers() {
-        return this.customerRepository.getAllCustomers();
+    @GetMapping("/customer")
+    public Collection<Customer> getAllCustomers(
+            @RequestParam(value = "offset", defaultValue = "0", required = false) String offset,
+            @RequestParam(value = "limit", required = false) String limit
+    ) {
+        try {
+            return this.customerRepository.getAllInRange(
+                    Integer.parseInt(offset),
+                    Integer.parseInt(limit)
+            );
+        } catch (NumberFormatException numberFormatException) {
+            return this.customerRepository.getAll();
+        }
     }
 
-    @PostMapping(value = "customer")
+    @PostMapping(path = "/customer")
     public Customer addNewCustomer(@RequestBody Customer customer) {
-        return null;
+        return this.customerRepository.add(customer);
     }
 
-    @GetMapping(value = "customer/id={customerId}")
+    @GetMapping(value = "/customer/id={customerId}")
     public Customer getCustomerById(@PathVariable String customerId) {
-        return this.customerRepository.getCustomerById(customerId);
+        return this.customerRepository.getById(customerId);
     }
 
-    @GetMapping(value = "customer/firstName={firstName}")
+    @GetMapping(value = "/customer/firstName={firstName}")
     public Collection<Customer> getCustomerByName(@PathVariable String firstName) {
-        return this.customerRepository.getCustomerByName(firstName);
+        return this.customerRepository.getByName(firstName);
     }
 
-    /*@PutMapping(value = "customer/{customerId}")
+    @PutMapping(value = "customer/update={customerId}")
     public Customer updateExistingCustomer(@PathVariable String customerId, @RequestBody Customer customer) {
-        return null;
+        return this.customerRepository.update(customer); // TODO: use customer ID
     }
 
-    @DeleteMapping(value = "customer/{customerId}")
+    @DeleteMapping(value = "customer/delete={customerId}")
     public boolean deleteCustomer(@PathVariable String customerId) {
-        return false;
-    }*/
+        return this.customerRepository.delete(customerId);
+    }
 }
