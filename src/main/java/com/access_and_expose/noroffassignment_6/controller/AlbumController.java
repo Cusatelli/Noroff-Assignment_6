7ee382dd289f8e94d322e5dd60d5afd172ae5090
Cusatelli <einarvandevelde@gmail.com>
2022-02-23 22:47:26 +0100

    REFACTOR: return Collection<Album> instead of singular Album.

--- src/main/java/com/access_and_expose/noroffassignment_6/controller/AlbumController.java
+++ src/main/java/com/access_and_expose/noroffassignment_6/controller/AlbumController.java
@@ -1,13 +1,15 @@
 package com.access_and_expose.noroffassignment_6.controller;
 
-import com.access_and_expose.noroffassignment_6.data.album.IAlbumRepository;
-import com.access_and_expose.noroffassignment_6.model.album.Album;
+import com.access_and_expose.noroffassignment_6.data.repository.album.IAlbumRepository;
+import com.access_and_expose.noroffassignment_6.model.Album;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
 
 @RestController
 @Tag(name = "Album")
@@ -26,7 +28,7 @@ public class AlbumController {
     }
 
     @GetMapping(value="/album/albumName={albumName}")
-    public Album getAlbumByName(@PathVariable String albumName) {
+    public Collection<Album> getAlbumByName(@PathVariable String albumName) {
         return this.albumRepository.getByName(albumName);
     }
 }
