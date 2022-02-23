2d0de2d26c4d1b94656c3e86af9955eeb23d8824
Anna H <70937358+Haruberi@users.noreply.github.com>
2022-02-23 15:52:59 +0100

    FEAT: implement methods on ArtistRepository.java

--- src/main/java/com/access_and_expose/noroffassignment_6/data/artist/ArtistRepository.java
+++ src/main/java/com/access_and_expose/noroffassignment_6/data/artist/ArtistRepository.java
@@ -1,15 +1,43 @@
 package com.access_and_expose.noroffassignment_6.data.artist;
-
+import com.access_and_expose.noroffassignment_6.data.SQLiteConnectionHelper;
 import com.access_and_expose.noroffassignment_6.model.artist.Artist;
 
-public class ArtistRepository implements IArtistRepository{
+import org.springframework.stereotype.Service;
+import java.sql.*;
+
+@Service
+public class ArtistRepository implements IArtistRepository {
+
     @Override
-    public Artist getArtistById(String artistId) {
-        return null;
+    public Artist getById(String artistId) {
+        String SQLQuery = "SELECT * FROM Artist WHERE ArtistId = ?";
+        return getArtist(SQLQuery, artistId);
     }
 
     @Override
-    public Artist getArtistByName(String artistName) {
-        return null;
+    public Artist getByName(String artistName) {
+        String SQLQuery = "SELECT * FROM Artist WHERE Name LIKE ?";
+        return getArtist(SQLQuery, artistName);
+    }
+
+    private Artist getArtist(String SQLQuery, String... params) {
+        Artist artists = null;
+        try (Connection connection = DriverManager.getConnection(SQLiteConnectionHelper.getConnectionString())) {
+            PreparedStatement preparedStatement = connection.prepareStatement(SQLQuery);
+            for (int i = 1; i < params.length + 1; i++) {
+                preparedStatement.setString(i, params[i - 1]);
+            }
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                artists = new Artist(
+                        resultSet.getLong("ArtistId"),
+                        resultSet.getString("Name")
+                );
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return artists;
     }
 }
