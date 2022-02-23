247905c0e9560ca6ee60c21db7bf4f229803289e
Anna H <70937358+Haruberi@users.noreply.github.com>
2022-02-23 15:44:14 +0100

    FEAT: add album Controller mappings

---
+++ src/main/java/com/access_and_expose/noroffassignment_6/controller/AlbumController.java
@@ -0,0 +1,32 @@
+package com.access_and_expose.noroffassignment_6.controller;
+
+import com.access_and_expose.noroffassignment_6.data.album.IAlbumRepository;
+import com.access_and_expose.noroffassignment_6.model.album.Album;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Tag(name = "Album")
+@RequestMapping(value="api/v1")
+public class AlbumController {
+
+    private final IAlbumRepository albumRepository;
+
+    public AlbumController(IAlbumRepository albumRepository) {
+        this.albumRepository = albumRepository;
+    }
+
+    @GetMapping(value = "/album/id={albumId}")
+    public Album getAlbumById(@PathVariable String albumId) {
+        return this.albumRepository.getById(albumId);
+    }
+
+    @GetMapping(value="/album/albumName={albumName}")
+    public Album getAlbumByName(@PathVariable String albumName) {
+        return this.albumRepository.getByName(albumName);
+    }
+}
