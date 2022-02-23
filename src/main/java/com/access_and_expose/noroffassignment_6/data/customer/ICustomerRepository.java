4b1143890f43ecd375b203f7857331edd86a13d4
Anna H <70937358+Haruberi@users.noreply.github.com>
2022-02-23 16:17:38 +0100

    REFACTOR: change name on Repository interface methods

--- src/main/java/com/access_and_expose/noroffassignment_6/data/customer/ICustomerRepository.java
+++ src/main/java/com/access_and_expose/noroffassignment_6/data/customer/ICustomerRepository.java
@@ -1,21 +1,19 @@
 package com.access_and_expose.noroffassignment_6.data.customer;
-
 import com.access_and_expose.noroffassignment_6.model.customer.Customer;
 import com.access_and_expose.noroffassignment_6.model.customer.CustomerCountry;
 import org.springframework.stereotype.Repository;
-
 import java.util.Collection;
 import java.util.HashMap;
-
 @Repository
 public interface ICustomerRepository {
 
-    Collection<Customer> getAllCustomers();
-    Customer getCustomerById(String customerId);
-    Collection<Customer> getCustomerByName(String firstName);
-    Customer addNewCustomer(Customer customer);
-    Customer updateExistingCustomer(Customer customer);
-    boolean deleteCustomer(String customerId);
+    Collection<Customer> getAll();
+    Collection<Customer> getAllInRange(int offset, int limit);
+    Customer getById(String customerId);
+    Collection<Customer> getByName(String firstName);
+    Customer add(Customer customer);
+    Customer update(Customer customer);
+    boolean delete(String customerId);
     Customer getCustomerHighestSpender();
     HashMap<CustomerCountry, Integer> getCustomersInCountry();
 }
