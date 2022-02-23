efd83e71328feac2abe935fdc80013b991db936f
Cusatelli <einarvandevelde@gmail.com>
2022-02-23 22:56:15 +0100

    FEAT: add Production & Development Connection Factory.

---
+++ src/main/java/com/access_and_expose/noroffassignment_6/data/factory/ProdConnectionFactory.java
@@ -0,0 +1,18 @@
+package com.access_and_expose.noroffassignment_6.data.factory;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+@Component
+@Profile("production")
+public class ProdConnectionFactory implements DatabaseConnectionFactory {
+    static final String URL = "jdbc:sqlite::resource:Northwind_small.sqlite";
+    @Override
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL);
+    }
+}
