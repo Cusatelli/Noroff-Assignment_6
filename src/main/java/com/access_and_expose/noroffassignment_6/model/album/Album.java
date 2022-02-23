3484c3e595719c2749e7a34f12cbc1d93ad20bc6
Anna H <70937358+Haruberi@users.noreply.github.com>
2022-02-23 15:41:33 +0100

    FEAT: add album attributes, constructor and getters/setters

--- src/main/java/com/access_and_expose/noroffassignment_6/model/album/Album.java
+++ src/main/java/com/access_and_expose/noroffassignment_6/model/album/Album.java
@@ -2,4 +2,33 @@ package com.access_and_expose.noroffassignment_6.model.album;
 
 public class Album {
 
+    private Long id;
+    private String title;
+
+    public Album() {}
+    public Album(Long id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "Album{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                '}';
+    }
 }
