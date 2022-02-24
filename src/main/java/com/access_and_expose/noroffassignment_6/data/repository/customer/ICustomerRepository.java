7c1aa0adb9919fe037e9727c73913c8650e06c99
Cusatelli <einarvandevelde@gmail.com>
2022-02-24 15:15:55 +0100

    REFACTOR: rename getSpender & add getFavoriteGenre.

--- src/main/java/com/access_and_expose/noroffassignment_6/data/repository/customer/ICustomerRepository.java
+++ src/main/java/com/access_and_expose/noroffassignment_6/data/repository/customer/ICustomerRepository.java
@@ -1,6 +1,8 @@
 package com.access_and_expose.noroffassignment_6.data.repository.customer;
 
 import com.access_and_expose.noroffassignment_6.data.repository.RestRepository;
+import com.access_and_expose.noroffassignment_6.model.Genre;
+import com.access_and_expose.noroffassignment_6.model.Order;
 import com.access_and_expose.noroffassignment_6.model.customer.Customer;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +10,7 @@ import java.util.LinkedHashMap;
 
 @Repository
 public interface ICustomerRepository extends RestRepository<Customer> {
-    LinkedHashMap<String, Integer> sortByCountry();
-    LinkedHashMap<Customer, Integer> getCustomerHighestSpender();
+    LinkedHashMap<String, Integer> sortByCountry(Order order);
+    LinkedHashMap<Customer, Integer> getSpender(Order order);
+    LinkedHashMap<Genre, Integer> getFavoriteGenre(String customerId);
 }
